229c0ce013f7335f5c5ade4f92a6fd376adbcae4
Diogo Naka <diogo.naka@invillia.com>
2023-02-16 18:36:07 -0300

    init git

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  :focus { }
+
+  body {
+    background: ${(props) => props.theme['background-color']};
+
+    color: ${(props) => props.theme['base-text']};
+    font-size: 1.6rem;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1.6rem;
+  }
+
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'base-title': '#272221',
+  'base-subtitle': '#403937',
+  'base-text': '#574F4D',
+  'base-label': '#8D8686',
+  'base-hover': '#D7D5D5',
+  'base-button': '#E6E5E5',
+  'base-input': '#EDEDED',
+  'base-card': '#F3F2F2',
+  'background-color': '#FAFAFA',
+
+  'yellow-100': '#F1E9C9',
+  'yellow-500': '#DBAC2C',
+  'yellow-700': '#C47F17',
+
+  'purple-100': '#EBE5F9',
+  'purple-500': '#8047F8',
+  'purple-700': '#4B2995',
+}
